e3124c0ed1dc392722d914d3db8dbe2972c1cb7f
akbel <colakakbel@gmail.com>
2019-12-05 19:10:18 +0100

    section 7 store class

--- app.js
+++ app.js
@@ -62,6 +62,36 @@ class UI {
 
 // Store Class: Handles Storage (here it is in browser)
 
+class Store {
+    static getBooks() {
+        let books;
+        if (localStorage.getItem('books') === null){
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+        
+        books.forEach((book, index) => {
+            if (book.isbn === isbn) {
+                book.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
 // Event : Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
