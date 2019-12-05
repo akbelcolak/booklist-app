27b55e07cba1f8d8d55f95ef2e5ab2b226614313
akbel <colakakbel@gmail.com>
2019-12-05 12:14:14 +0100

    section6  Alerts

--- app.js
+++ app.js
@@ -41,6 +41,18 @@ class UI {
         }
     }
 
+    static showAlert (message,className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div,form);
+        
+        // vanish in 2 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 2000);
+    }
+
     static clearFields(){
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -64,18 +76,30 @@ document.querySelector('#book-form').addEventListener('submit', (e) =>{
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
+    // validate
+    if(title === ''  || author === '' || isbn === ''){
+        UI.showAlert('please fill in all fields', 'danger');
+    } else{
+    
     // instantiate book
     const book = new Book(title,author,isbn);
     
     // add Book to UI
     UI.addBookToList(book);
 
+    // Show success message
+    UI.showAlert('Book Added', 'success');
+
     // Clear Fields
     UI.clearFields();
-})
+    } 
+});
 
 // Event : Remove a Book (from storege and UI)
 document.querySelector('#book-list').addEventListener('click', (e) =>
 {
-    UI.deleteBook(e.target)
+    UI.deleteBook(e.target);
+
+    // Show success message
+    UI.showAlert('Book Removed', 'info');
 })
